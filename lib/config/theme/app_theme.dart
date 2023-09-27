4ea8b1a48abb69b371414b8c91f3e441a840d13c
alejo Gonzalez <alejo.g31@hotmail.com>
2023-09-26 23:34:29 -0500

    Se agregan dependencias y archivo config

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.pink,
+  Colors.pinkAccent
+];
+
+class AppTheme {
+  final int selectColor;
+  final bool isDarkmode;
+  AppTheme({this.isDarkmode = false, this.selectColor = 0})
+      : assert(selectColor >= 0, 'Selected color must be greater then 0'),
+        assert(selectColor < colorList.length,
+            'Selected color must be lees or equal than ${colorList.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        brightness: isDarkmode ? Brightness.dark : Brightness.light,
+        colorSchemeSeed: colorList[selectColor],
+        appBarTheme: const AppBarTheme(centerTitle: false),
+      );
+
+  //Creando metodo q regresa una instancia de AppTheme
+  AppTheme copyWhith({
+    int? selectedColor,
+    bool? isDarkmode,
+  }) =>
+      AppTheme(
+          selectColor: selectedColor ?? selectColor,
+          isDarkmode: isDarkmode ?? this.isDarkmode);
+}
