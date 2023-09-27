7b40fda12f931e05a196fe6123199f8d66bc3d18
alejo Gonzalez <alejo.g31@hotmail.com>
2023-09-26 23:49:29 -0500

    Se termina de configurar main y bases del proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:vetter_app/config/router/app_router.dart';
+import 'package:vetter_app/config/theme/app_theme.dart';
+import 'package:vetter_app/src/providers/theme/theme_provider.dart';
 
 void main() {
-  runApp(
-    const MyApp(),
-  );
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => MyAppState();
-}
-
-class MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, ref) {
+    final AppTheme appTheme = ref.watch(themeNotifierProvider);
+    return MaterialApp.router(
+      title: 'Vetter app',
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      theme: appTheme.getTheme(),
+    );
   }
 }
---
+++ lib/src/providers/theme/theme_provider.dart
@@ -0,0 +1,29 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:vetter_app/config/theme/app_theme.dart';
+
+final isDarkmodeProvider = StateProvider((ref) => false);
+
+//Listado de colores inmutable Provider -> para valores inmutables
+final colorListProvider = Provider((ref) => colorList);
+
+//Un simple int StateProvider -> para cambios de estado
+final selectedColorProvider = StateProvider((ref) => 0);
+
+//Creando  un objeto de tipo AppTheme -> para objetos mas elavorados
+final themeNotifierProvider = StateNotifierProvider<ThemeNotifier, AppTheme>(
+  (ref) => ThemeNotifier(),
+);
+
+//Controller o Notifier
+class ThemeNotifier extends StateNotifier<AppTheme> {
+  //super(AppTheme()) Se le indica que cree la primera instancia de AppTheme
+  ThemeNotifier() : super(AppTheme());
+
+  void toogleDarkmode() {
+    state = state.copyWhith(isDarkmode: !state.isDarkmode);
+  }
+
+  void changeColorIndex(int colorIndex) {
+    state = state.copyWhith(selectedColor: colorIndex);
+  }
+}
